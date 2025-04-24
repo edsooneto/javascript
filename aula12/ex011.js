e5364185a23d0eff14ceaaac7af834ee99da7d19
edsooneto <168638099+edsooneto@users.noreply.github.com>
2025-04-24 09:44:35 -0300

    Atualizações estudos JS

---
+++ aula12/ex011.js
@@ -0,0 +1,13 @@
+var idade = 66
+console.log(`Você tem ${idade} anos`)
+if (idade < 16) {
+    console.log('Menor de Idade')
+
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto Opcional')
+
+} else {
+    console.log('Voto obrigatório')
+}
+
+// CONDIÇÃO ANINHADA
