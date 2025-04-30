1a4acfc70dcc0b74ee147e9a50a93975531cd137
edsooneto <168638099+edsooneto@users.noreply.github.com>
2025-04-30 09:55:50 -0300

    Atualizações JS

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,9 +1,9 @@
-function carregar() {
+function carregar () {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora} Horas!`
 
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
